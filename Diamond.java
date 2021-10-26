86556f7b9c567d65e9348491644ad10dc7a10ecb
Oudone Phengkhamlar <84829747+OudonePKL@users.noreply.github.com>
2021-10-26 13:03:35 +0700

    Update Diamond.java

--- Diamond.java
+++ Diamond.java
@@ -4,21 +4,21 @@ public class Diamond {
         
         int a = 6;
 
-        for(int i=1; i<=a; i++){
-            for(int j=i; j<a; j++){
+        for(int i = 1; i <= a; i++){
+            for(int j = i; j < a; j++){
                 System.out.print(" ");
             }
-            for(int j=1; j<i*2; j++){
+            for(int j = 1; j < (i*2); j++){
                 System.out.print("*");
             }
             System.out.println();
         }
 
-        for(int i=a-1; i>=1; i--){
-            for(int j=a; j>i; j--){
+        for(int i = a-1; i >= 1; i--){
+            for(int j = a; j > i; j--){
                 System.out.print(" ");
             }
-            for(int j=1; j<i*2; j++){
+            for(int j = 1; j < i*2; j++){
                 System.out.print("*");
             }
             System.out.println();
